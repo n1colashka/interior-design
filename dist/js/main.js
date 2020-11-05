344141a692867e1806a8698e71e801c2cdc42fbb
Nick <deckards2505@gmail.com>
2020-11-05 19:54:20 +0600

    feat: add works, service sections

--- dist/js/main.js
+++ dist/js/main.js
@@ -26,6 +26,119 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   }
 
+  function initTabs() {
+    var $tabs = document.querySelector('.works__tabs');
+    var $tabsElems = document.querySelectorAll('.works__tab');
+    var $tabsItems = document.querySelectorAll('.works__item');
+
+    if ($tabs) {
+      $tabs.addEventListener('click', function (event) {
+        if (event.target.classList.contains('works__tab')) {
+          $tabsElems.forEach(function (tab) {
+            tab.classList.remove('active');
+          });
+          $tabsItems.forEach(function (item) {
+            if (event.target.dataset.tab === item.dataset.tab) {
+              item.classList.add('active');
+              event.target.classList.add('active'); // galleryTop2.init();
+              // galleryThumbs2.init();
+            } else {
+              item.classList.remove('active');
+            }
+          });
+        }
+      });
+    }
+  }
+
+  function initWorksSliders() {
+    if (document.querySelector('.works__slider')) {
+      var galleryThumbs1 = new Swiper('.works__slider-thumbs--1', {
+        spaceBetween: 5,
+        slidesPerView: 7,
+        freeMode: true,
+        watchSlidesVisibility: true,
+        watchSlidesProgress: true,
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev'
+        },
+        observer: true,
+        observeParents: true,
+        observeSlideChildren: true
+      });
+      var galleryTop1 = new Swiper('.works__slider--1', {
+        spaceBetween: 10,
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev'
+        },
+        thumbs: {
+          swiper: galleryThumbs1
+        },
+        observer: true,
+        observeParents: true,
+        observeSlideChildren: true
+      });
+      var galleryThumbs2 = new Swiper('.works__slider-thumbs--2', {
+        spaceBetween: 5,
+        slidesPerView: 7,
+        freeMode: true,
+        watchSlidesVisibility: true,
+        watchSlidesProgress: true,
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev'
+        },
+        observer: true,
+        observeParents: true,
+        observeSlideChildren: true
+      });
+      var galleryTop2 = new Swiper('.works__slider--2', {
+        spaceBetween: 10,
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev'
+        },
+        thumbs: {
+          swiper: galleryThumbs2
+        },
+        observer: true,
+        observeParents: true,
+        observeSlideChildren: true
+      });
+      var galleryThumbs3 = new Swiper('.works__slider-thumbs--3', {
+        spaceBetween: 5,
+        slidesPerView: 7,
+        freeMode: true,
+        watchSlidesVisibility: true,
+        watchSlidesProgress: true,
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev'
+        },
+        observer: true,
+        observeParents: true,
+        observeSlideChildren: true
+      });
+      var galleryTop3 = new Swiper('.works__slider--3', {
+        spaceBetween: 10,
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev'
+        },
+        thumbs: {
+          swiper: galleryThumbs3
+        },
+        observer: true,
+        observeParents: true,
+        observeSlideChildren: true
+      });
+    }
+  }
+
   initMenu();
   initHeaderAccordions();
+  initTabs();
+  initWorksSliders();
 });
